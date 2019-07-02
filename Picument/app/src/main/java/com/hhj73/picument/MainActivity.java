171cf8c552f4e231e77f9a743f17424a05801d14
Hyejin Hong <hhj732@naver.com>
2019-07-02 20:20:59 +0900

    openCV native 설정

---
+++ Picument/app/src/main/java/com/hhj73/picument/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hhj73.picument;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+//    public native String stringFromJNI();
+
+    public native void detectEdgeJNI(long inputImage, long outputImage, int th1, int th2);
+}
