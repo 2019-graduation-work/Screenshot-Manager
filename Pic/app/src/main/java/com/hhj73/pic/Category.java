aee3fd20ca18a4b37df60fd8f3c064ccab96f9da
Hyejin Hong <hhj732@naver.com>
2019-07-30 11:13:58 +0900

    권한 추가

---
+++ Pic/app/src/main/java/com/hhj73/pic/Category.java
@@ -0,0 +1,8 @@
+package com.hhj73.pic;
+
+import java.util.ArrayList;
+
+public class Category { // 분류될 카테고리
+    String name; // 카테고리 이름
+    ArrayList<Picture> pictures;
+}
---
+++ Pic/app/src/main/java/com/hhj73/pic/Picture.java
@@ -0,0 +1,38 @@
+package com.hhj73.pic;
+
+import java.util.Date;
+
+public class Picture { // 사진
+    String name; // 이름
+    String text; // 추출한 text
+    Date date;
+
+    public Picture(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
