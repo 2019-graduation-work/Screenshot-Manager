ca2e16181c647f4e25e35059b7e7ea060dcc0012
Hyejin Hong <hhj732@naver.com>
2019-08-16 16:58:43 +0900

    툴바 생성

--- Pic/app/src/main/java/com/hhj73/pic/Main2Activity.java
+++ Pic/app/src/main/java/com/hhj73/pic/Main2Activity.java
@@ -3,6 +3,9 @@ package com.hhj73.pic;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,7 +14,31 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        // 툴바
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.myAppName);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(toolbar_menu);
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.search:
+                // Do something
+                return true;
+            case R.id.option :
+                // Do something
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
