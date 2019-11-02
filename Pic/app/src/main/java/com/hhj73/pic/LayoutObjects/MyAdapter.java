2b5f96db8649532827c9d5d3f33a3ee8c63bb092
Hyejin Hong <hhj732@naver.com>
2019-11-03 00:19:53 +0900

    Add click event on cardview

--- Pic/app/src/main/java/com/hhj73/pic/LayoutObjects/MyAdapter.java
+++ Pic/app/src/main/java/com/hhj73/pic/LayoutObjects/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.hhj73.pic.LayoutObjects;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.hhj73.pic.ImageViewActivity;
 import com.hhj73.pic.Objects.Picture;
 import com.hhj73.pic.R;
 
@@ -18,21 +20,31 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private ArrayList<Picture> mDataSet;
-    private Context mContext;
+    public static ArrayList<Picture> mDataSet;
+    public static Context mContext;
 
     public MyAdapter(ArrayList<Picture> mDataSet, Context mContext) {
         this.mDataSet = mDataSet;
         this.mContext = mContext;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public ImageView mImageView;
         public LinearLayout mLinearLayout;
         public ViewHolder(View v) {
             super(v);
             mImageView = (ImageView) v.findViewById(R.id.iv);
             mLinearLayout = (LinearLayout) v.findViewById(R.id.ll);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+//            Toast.makeText(v.getContext(), "힝"+getLayoutPosition(), Toast.LENGTH_SHORT).show();
+            Intent intent1 = new Intent(v.getContext(), ImageViewActivity.class);
+            intent1.putExtra("picture", mDataSet.get(getLayoutPosition()));
+            v.getContext().startActivity(intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
         }
     }
 
@@ -62,4 +74,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public int getItemCount() {
         return mDataSet.size();
     }
+
+
 }
